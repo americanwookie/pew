8101e5b2960e5c81d53f8267b81385d0ba734cee
Scott O'Neil <scottgit@bitshouston.com>
2014-12-29 11:37:52 -0600

    mark-read.pl: Adding support for multiple mailboxes

--- mark-read.pl
+++ mark-read.pl
@@ -19,19 +19,24 @@ if(!$imap->login( $config->{'server'}->{'user'}, $config->{'server'}->{'pass'} )
 
 my %mailboxes = map { $_ => 1 } $imap->mailboxes();
 my $mailbox;
-if( $ARGV[0] ) {
-  die( $ARGV[0].' isn\'t a mailbox. Run without an argument for a list of mailboxes' ) if( !exists( $mailboxes{$ARGV[0]} ) );
-  $mailbox = $ARGV[0];
-} else {
-  print "The first argument to this command must be a mailbox name. Here is a list of mailboxes:\n";
+if( !@ARGV ) {
+  print "This command accepts a list of mailboxes as its argument. Here is a list of mailboxes:\n";
   print join("\n", sort keys( %mailboxes ) )."\n";
   exit(1);
 }
 
-$imap->select( $mailbox );
-my @ids = $imap->search("UNSEEN");
-if($imap->see( Util::list2range( @ids ) ) ) {
-  print "Marked ".scalar @ids." messages as read\n";
-} else {
-  print "Problems encountered when trying to mark ".scalar @ids." as read\n";
+foreach my $mailbox ( @ARGV ) {
+  die( $mailbox.' isn\'t a mailbox. Run without an argument for a list of mailboxes' ) if( !exists( $mailboxes{$mailbox} ) );
+
+  $imap->select( $mailbox );
+  my @ids = $imap->search("UNSEEN");
+  if( @ids ) {
+    if($imap->see( Util::list2range( @ids ) ) ) {
+      print "Marked ".scalar @ids." messages as read in $mailbox\n";
+    } else {
+      print "Problems encountered when trying to mark ".scalar @ids." messages as read in $mailbox\n";
+    }
+  } else {
+    print "No unread messages found in $mailbox\n";
+  }
 }
